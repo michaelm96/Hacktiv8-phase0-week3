e00f2b37eee0bfab6a6fd9473d1f91a144eddab2
Michael M <47624034+michaelm96@users.noreply.github.com>
2020-02-27 16:54:44 +0700

    Add files via upload

---
+++ tugasRabu/palindrome.js
@@ -0,0 +1,16 @@
+function palindrome(kata) {
+    var char = kata.split("");
+    var word = ""
+    for(var i=0; i< kata.length; i++){
+        console.log(char);
+        
+    }
+  }
+  
+  // TEST CASES
+  console.log(palindrome('katak')); // true
+  console.log(palindrome('blanket')); // false
+  console.log(palindrome('civic')); // true
+  console.log(palindrome('kasur rusak')); // true
+  console.log(palindrome('mister')); // false
+
