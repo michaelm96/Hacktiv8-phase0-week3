6f41737f7383788e381c4e139086dcd0f5922da0
Michael M <47624034+michaelm96@users.noreply.github.com>
2020-03-04 22:50:52 +0700

    Add files via upload

--- tugasRabu/palindromeAngka.js
+++ tugasRabu/palindromeAngka.js
@@ -1,10 +1,14 @@
 function angkaPalindrome(num) {
-    var numStr = num.toString();
-    for(var i = 0; i < numStr.length; i++){
-        if(numStr[i] === numStr[numStr.length - 1]){
-            var toNum = parseInt(numStr);
-            console.log(typeof numStr);
-        }
+    while(num != 0){
+      var balik=""
+      num++
+      var strNum = num.toString()
+      for (var i=strNum.length-1; i>=0; i--){
+        balik+=strNum[i]
+      }
+      if (balik == num){
+        return num
+      }
     }
   }
   
@@ -12,5 +16,5 @@ function angkaPalindrome(num) {
   console.log(angkaPalindrome(8)); // 9
   console.log(angkaPalindrome(10)); // 11
   console.log(angkaPalindrome(117)); // 121
-  console.log(angkaPalindrome(175)); // 181
+  console.log(angkaPalindrome(1234567890)); // 181
   console.log(angkaPalindrome(1000)); // 1001
